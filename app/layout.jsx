2e8e36376dc17f97fb3265051aa608fa41b6584d
Arya4711 <armlk4711@gmail.com>
2024-04-25 11:40:46 -0400

    Add smooth scrolling

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <link rel="icon" href="/favicon.svg" sizes="any" />
         <meta name="darkreader-lock" />
