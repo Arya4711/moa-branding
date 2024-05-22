069f76e6391d758e10bf3dc785b40b4db889cc83
Arya4711 <armlk4711@gmail.com>
2024-05-22 12:33:40 -0400

    finalize contact page
    add header and text at bottom for scheduling a meeting or sending an email

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,6 @@
 import NavBar from "../_ui/NavBar";
 import Footer from "../_ui/Footer";
-import FormInput from "../_ui/FormInput";
+import FormInput from "./_ui/FormInput";
 
 export default function Contact() {
   return (
@@ -53,6 +53,29 @@ export default function Contact() {
             />
           </form>
         </div>
+        <p className="mx-auto text-xl">
+          If you are having any issues with submitting this form, please feel
+          free to send us an email at{" "}
+          <span className="text-sky-500">
+            <a
+              className="underline decoration-sky-500 transition-all hover:decoration-transparent"
+              href="mailto:moabrandingfl@gmail.com"
+            >
+              moabrandingfl@gmail.com
+            </a>
+          </span>{" "}
+          or{" "}
+          <span className="text-sky-500">
+            <a
+              className="underline decoration-sky-500 transition-all hover:decoration-transparent"
+              href="https://calendar.app.google/WJ1hAdc2iSTD9hkm8"
+              target="_blank"
+            >
+              book a meeting
+            </a>
+          </span>{" "}
+          with us.
+        </p>
         <Footer />
       </div>
     </>
