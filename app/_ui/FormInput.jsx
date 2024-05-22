f80974eaf9d4bc89e1800bd7fbaf51cd054dae2f
Arya M <armlk4711@gmail.com>
2024-05-22 11:56:39 -0400

    add additional fields to form inputs and use formsubmit.co to handle form submission

--- app/_ui/FormInput.jsx
+++ app/_ui/FormInput.jsx
@@ -1,9 +1,14 @@
-export default function FormInput({ placeholder }) {
+export default function FormInput({ placeholder, form, name, type = "text" }) {
   return (
     <input
       className="block rounded-full bg-neutral-200 px-5 py-2 outline outline-1 outline-neutral-400 transition hover:brightness-95 focus:bg-sky-50 focus:outline-sky-300"
       placeholder={placeholder}
+      form={form}
+      name={name}
       size="70"
+      maxLength="30"
+      type={type}
+      required
     ></input>
   );
 }
