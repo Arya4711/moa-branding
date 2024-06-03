0a07c5e028ed67924de24301ae8fd86592d3f082
Arya M <armlk4711@gmail.com>
2024-06-03 17:06:26 -0400

    add portfolio box component

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -6,12 +6,29 @@ export default function Portfolio() {
     <>
       <SectionButton text="PORTFOLIO" id="portfolio" />
       <div className="mx-20 mb-10 flex justify-between">
-        <PortfolioBox image="moa-branding-logo" text="Our Brand" link="#" />
-        <PortfolioBox image="logo-redesign" text="Logo Redesign" link="#" />
+        <PortfolioBox
+          image="moa-branding-logo"
+          labelText="Our Brand"
+          contentText="We used vector graphics and wireframing on Figma and Illustrator to construct our website and design our logo. We also chose our color palettes and created our slogan in-house."
+          link="#"
+        />
+        <PortfolioBox
+          image="logo-redesign"
+          labelText="Logo Redesign"
+          link="#"
+        />
       </div>
       <div className="mx-20 mb-10 flex justify-between">
-        <PortfolioBox image="logo-redesign" text="Logo Redesign" link="#" />
-        <PortfolioBox image="logo-redesign" text="Logo Redesign" link="#" />
+        <PortfolioBox
+          image="logo-redesign"
+          labelText="Logo Redesign"
+          link="#"
+        />
+        <PortfolioBox
+          image="logo-redesign"
+          labelText="Logo Redesign"
+          link="#"
+        />
       </div>
     </>
   );
