2c5a8f725e163432af340dd56f9fc7c589f2a750
Arya M <armlk4711@gmail.com>
2024-06-03 17:11:38 -0400

    add image translate breakpoint styling for 4k screens

--- app/about/_ui/MemberImage.jsx
+++ app/about/_ui/MemberImage.jsx
@@ -16,19 +16,21 @@ export default function MemberImage({
       alt={alt}
       width={300}
       height={300}
-      className={`${selected && selected !== toCompare
-        ? hasTransitioned
-          ? "hidden"
-          : "opacity-0"
-        : ""
-        } ${selected !== 1 && selected === toCompare
+      className={`${
+        selected && selected !== toCompare
+          ? hasTransitioned
+            ? "hidden"
+            : "opacity-0"
+          : ""
+      } ${
+        selected !== 1 && selected === toCompare
           ? hasTransitioned
             ? "transition-none"
             : selected === 2
-              ? "2xl:-translate-x-[36.5rem] md:max-2xl:-translate-x-[23rem]"
-              : "2xl:-translate-x-[73.25rem] md:max-2xl:-translate-x-[46rem]"
+              ? "md:max-2xl:-translate-x-[23rem] 2xl:max-[2500px]:-translate-x-[36.5rem] min-[2500px]:-translate-x-[50rem]"
+              : "md:max-2xl:-translate-x-[46rem] 2xl:max-[2500px]:-translate-x-[73.25rem] min-[2500px]:-translate-x-[100rem]"
           : ""
-        } m-auto transition-all duration-500 row-span-3 z-10 md:max-2xl:w-60`}
+      } z-10 row-span-3 m-auto transition-all duration-500 md:max-2xl:w-60`}
       onMouseEnter={() => {
         if (!selected) setSelected(toCompare);
       }}
